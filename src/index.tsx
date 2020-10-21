5d832104f951d4bb26bab11f3346b1627913af90
Sergio Cano <serdeliverance@gmail.com>
2020-10-21 19:16:30 -0300

    typescript way of defining components using generics

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,15 @@ interface AppProps {
   color?: string
 }
 
-class App extends React.Component<AppProps> {
-  state = {
-    counter: 0
+interface AppState {
+  counter: number
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props)
+
+    this.state = { counter: 0 }
   }
 
   onIncrement = (): void => {
